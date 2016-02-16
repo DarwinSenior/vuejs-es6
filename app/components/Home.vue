93a05795a61c22daa5108fda5cdcfd3abd2449b4
Martin Ronquillo <ronquillo.m@gmail.com>
2016-02-15 22:00:26 -0700

    Added sample data and layout for displaying todos

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -9,10 +9,27 @@
                 <div class="pure-u-4-24 pure-u-md-1-3"></div>
             </div>
         </form>
+        <div class="pure-g">
+            <div class="pure-u-4-24 pure-u-md-1-3"></div>
+            <div class="pure-u-16-24 pure-u-md-1-3">
+                <div class="pure-u-1" v-for="item in items">
+                    <input type="checkbox" v-model="item.checked">
+                    {{ item.content }}
+                </div>
+            </div>
+            <div class="pure-u-4-24 pure-u-md-1-3"></div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            items: [
+                { content: 'Sample content', checked: false }
+            ]
+        }
+    }
 }
 </script>
