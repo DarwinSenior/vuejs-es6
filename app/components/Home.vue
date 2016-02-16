8abbee7fb9c8838ca2f38ebaa976267213757ddf
Martin Ronquillo <ronquillo.m@gmail.com>
2016-02-15 21:48:34 -0700

    Home view layout

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -2,11 +2,11 @@
     <div>
         <form class="pure-form">
             <div class="pure-g">
-                <div class="pure-u-1-3"></div>
-                <div class="pure-u-1-3">
+                <div class="pure-u-4-24 pure-u-md-1-3"></div>
+                <div class="pure-u-16-24 pure-u-md-1-3">
                     <input class="pure-input-1" type="text" placeholder="I need to...">
                 </div>
-                <div class="pure-u-1-3"></div>
+                <div class="pure-u-4-24 pure-u-md-1-3"></div>
             </div>
         </form>
     </div>
